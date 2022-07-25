2c79d84ceded9072ae8de52eb351366d5c537200
walter Rodriguez <walter.rodriguez1992@gmail.com>
2022-07-25 17:12:17 -0500

    mejorando navbar

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired
 #se crea la clase para usar Flask_wtf
 class LoginForm(FlaskForm):
     username = StringField("Nombre de usuario", validators= [DataRequired()])
-    password = PasswordField("Password", validators= [DataRequired()])
+    password = PasswordField("Contraseña", validators= [DataRequired()])
     submit =SubmitField("Iniciar Sesión")
 
 class TodoForm(FlaskForm):
-    description = StringField("Descripcion", validators = [DataRequired()])
+    description = StringField("Descripción", validators = [DataRequired()])
     submit = SubmitField("Crear")
 
 class DeleteTodoForm(FlaskForm):
